53b5ef31be632e0f81727b41c47f88ea281410ef
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-12-04 11:50:42 -0300

    .

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): JSX.Element {
+  return <Text>ola</Text>
+    
+
+}
+
+export default App;
---
+++ app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Text } from "react-native";
+
+function App(){
+    return <Text>ola</Text>
+}
+
+export default App;

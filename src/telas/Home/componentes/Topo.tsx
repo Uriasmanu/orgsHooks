385f09d2ae1ead40a311f62816acac1b22e7970e
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-12-04 23:31:55 -0300

    .

--- src/telas/Home/componentes/Topo.tsx
+++ src/telas/Home/componentes/Topo.tsx
@@ -1,14 +1,38 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-import logo from '../../../../assets/logo.png';
+import logo from '../../../assets/logo.png';
 
 function Topo(): JSX.Element {
   return (
-    <View>
-      <Image source={logo} />
+    <View style={estilos.topo}>
+      <Image style={estilos.imagem} source={logo} />
+      <Text style={estilos.boasVindas}>Ola Manoela</Text>
+      <Text style={estilos.legenda}>Encontre os melhores produtores</Text>
     </View>
   );
 }
+
+const estilos = StyleSheet.create({
+  topo:{
+    backgroundColor: "#f6f6f6",
+    padding: 16
+  },
+  imagem:{
+    width: 70,
+    height: 28
+  },
+  boasVindas:{
+    marginTop:24,
+    fontSize:26,
+    lineHeight: 42,
+    fontWeight:'bold'
+  },
+  legenda:{
+    fontSize: 16,
+    lineHeight: 26,
+  }
+})
+
 
 export default Topo;
